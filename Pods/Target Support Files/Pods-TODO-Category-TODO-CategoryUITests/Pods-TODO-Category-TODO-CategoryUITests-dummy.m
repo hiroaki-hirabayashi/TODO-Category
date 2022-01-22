19d894e63b32ac38a391a86f91d8a8abf4165e5a
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-01-22 23:44:20 +0900

    ColorAssets追加

---
+++ Pods/Target Support Files/Pods-TODO-Category-TODO-CategoryUITests/Pods-TODO-Category-TODO-CategoryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TODO_Category_TODO_CategoryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TODO_Category_TODO_CategoryUITests
+@end
